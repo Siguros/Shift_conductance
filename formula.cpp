40e19d4dcb4efb23720f50ac4fbb25f421841511
Siguros <47727992+Siguros@users.noreply.github.com>
2021-02-06 21:42:51 +0900

    Update formula.cpp

--- formula.cpp
+++ formula.cpp
@@ -321,10 +321,11 @@ double NonlinearConductance(double C, double NL, double Vw, double Vr, double V)
 	double C_NL = C * Vr/V * pow(NL, (V-Vr)/(Vw/2));
 	return C_NL;
 }
-double getLinear(double A, double Pmax)
+double getLinear(double A, double Pmax,double B)
 {
 	
-	return A * log(Pmax / (1 - exp(-Pmax / A)));
+	//return A * log(Pmax / (1 - exp(-Pmax / A)));
+	return A*log(B/A);
 }
 
 double getSymmetric(double Altp, double Pmaxltp, double Altd, double Pmaxltd)
